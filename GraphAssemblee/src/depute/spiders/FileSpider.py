84e1ba885e2e35319d95cd877fc02c025de0ad80
spgenot <simon-pierre.genot@epfl.ch>
2015-04-04 15:46:31 +0100

    Correct json

--- GraphAssemblee/src/depute/spiders/FileSpider.py
+++ GraphAssemblee/src/depute/spiders/FileSpider.py
@@ -7,7 +7,10 @@ class FileSpider(scrapy.Spider):
     name='LinkSpider'
     allowed_domains = 'http://www.elus20.fr/'
     start_urls = ['http://www.elus20.fr/deputes-web-facebook-twitter/']
-    
+    arch_urls = 'http://www.elus20.fr/deputes-web-facebook-twitter/?limitstart='
+    for i in range(17,700,17):
+        start_urls.append(arch_urls + str(i))
+        
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
         link = hxs.select('//td//div[@class="cbUserListFieldLine"]//span[@class="cbListFieldCont cbUserListFC_name"]//a[@href]').extract()
