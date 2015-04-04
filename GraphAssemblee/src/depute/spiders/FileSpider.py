4a4dda6806a86b1887e13600ccd0be7c57ded754
spgenot <simon-pierre.genot@epfl.ch>
2015-04-04 15:38:05 +0100

    Corrected LinkSpider

--- GraphAssemblee/src/depute/spiders/FileSpider.py
+++ GraphAssemblee/src/depute/spiders/FileSpider.py
@@ -1,18 +1,18 @@
 import scrapy
 from depute.items import DeputeItem
 from scrapy.selector import HtmlXPathSelector
+from scrapy import log
+
 class FileSpider(scrapy.Spider):
     name='LinkSpider'
     allowed_domains = 'http://www.elus20.fr/'
-    start_url = ['http://www.elus20.fr/deputes-web-facebook-twitter/?limitstart=17']
-    #arch_url = 'http://www.elus20.fr/deputes-web-facebook-twitter/?limitstart='
+    start_urls = ['http://www.elus20.fr/deputes-web-facebook-twitter/']
+    
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-        link = hxs.select('//td//div[@class="cbUserListFieldLine"]//span[@class="cbListFieldCont cbUserListFC_name"]//a[@href]')
-        #for sel in response.xpath('//td//div[@class="cbUserListFieldLine"]'):
+        link = hxs.select('//td//div[@class="cbUserListFieldLine"]//span[@class="cbListFieldCont cbUserListFC_name"]//a[@href]').extract()
         for l in link:
             item = DeputeItem()
-            item['link'] = l#sel.xpath('//span[@class="cbListFieldCont cbUserListFC_name"]//a[@href]').extract()
-            #item['partie']=sel.xpath('//span[@class="cbListFieldCont cbUserListFC_avatar"]//img//@alt').extract()
+            item['link'] = l
             yield item
         
